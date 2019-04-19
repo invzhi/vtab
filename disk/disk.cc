60cbd7edde9c991095b637d9106ba607119b2afa
invzhi <invzhi@protonmail.com>
2019-04-19 21:24:56 +0800

    add disk

---
+++ disk/disk.cc
@@ -0,0 +1,37 @@
+#include "disk/disk.h"
+
+#include <iostream>
+#include <string>
+
+#include "spdlog/spdlog.h"
+#include "page/page.h"
+
+Disk::Disk(const std::string &db_file) {
+  auto mode = std::ios_base::in | std::ios_base::out | std::ios_base::binary;
+  db_io_.open(db_file, mode);
+  if (!db_io_) {
+    spdlog::error("cannot open file: {}", db_file);
+  }
+}
+
+PageID Disk::AllocatePage() {
+  return next_page_id_++;
+}
+
+void Disk::DeallocatePage(PageID page_id) {
+  // TODO: bitmap in page directory, manage free pages
+}
+
+void Disk::ReadPage(PageID page_id, char *data) {
+  assert(page_id < next_page_id_);
+
+  int offset = page_id * PAGE_SIZE;
+  db_io_.seekg(offset);
+  db_io_.read(data, PAGE_SIZE);
+}
+
+void Disk::WritePage(PageID page_id, const char *data) {
+  int offset = page_id * PAGE_SIZE;
+  db_io_.seekp(offset);
+  db_io_.write(data, PAGE_SIZE);
+}
---
+++ disk/disk.h
@@ -0,0 +1,24 @@
+#ifndef VTAB_DISK_H
+#define VTAB_DISK_H
+
+#include <string>
+#include <fstream>
+
+#include "page/page.h"
+
+class Disk {
+ public:
+  Disk(const std::string &db_name);
+
+  PageID AllocatePage();
+  void DeallocatePage(PageID page_id);
+
+  void ReadPage(PageID page_id, char *data);
+  void WritePage(PageID page_id, const char *data);
+
+ private:
+  std::fstream db_io_; // TODO: change to O_DIRECT, without internal buffer
+  PageID next_page_id_ = 0;
+};
+
+#endif
