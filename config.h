f0495c7ad0087c29defb24d8078b2d02fcd44fe3
invzhi <invzhi@protonmail.com>
2019-04-19 21:24:27 +0800

    add config.h, page.h

---
+++ config.h
@@ -0,0 +1,7 @@
+#ifndef VTAB_CONFIG_H
+#define VTAB_CONFIG_H
+
+#define PAGE_SIZE 4096
+#define BUFFER_POOL_SIZE 204800
+
+#endif
---
+++ page/page.h
@@ -0,0 +1,21 @@
+#ifndef VTAB_PAGE_H
+#define VTAB_PAGE_H
+
+#include "config.h"
+
+using PageID = int32_t;
+
+class Page {
+  friend class BufferPool;
+ public:
+  PageID page_id() const { return page_id_; }
+  char *data() { return data_; }
+
+ private:
+  PageID page_id_;
+  int pin_count_ = 0;
+  bool is_dirty_ = false;
+  char data_[PAGE_SIZE];
+};
+
+#endif
