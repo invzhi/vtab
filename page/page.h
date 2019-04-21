6b2a5845629f7b201635571839bce1e919fcd6cc
invzhi <invzhi@protonmail.com>
2019-04-21 20:36:31 +0800

    page: add mutex to protect data

--- page/page.h
+++ page/page.h
@@ -1,13 +1,21 @@
 #ifndef VTAB_PAGE_PAGE_H
 #define VTAB_PAGE_PAGE_H
 
+#include <mutex>
+
 #include "config.h"
 
 using PageID = int32_t;
 
 class Page {
   friend class BufferPool;
+
  public:
+  void RLock() { mutex_.lock(); }
+  void RUnlock() { mutex_.unlock(); }
+  void WLock() { mutex_.lock(); }
+  void WUnlock() { mutex_.unlock(); }
+
   PageID page_id() const { return page_id_; }
   char *data() { return data_; }
 
@@ -15,6 +23,8 @@ class Page {
   PageID page_id_;
   int pin_count_ = 0;
   bool is_dirty_ = false;
+
+  std::mutex mutex_;
   char data_[PAGE_SIZE];
 };
 
