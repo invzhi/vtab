0d2b38a9c9522aa852b9f4f661ae093abc3796e5
invzhi <invzhi@protonmail.com>
2019-04-22 18:04:51 +0800

    table: add row_id.h

---
+++ table/row_id.h
@@ -0,0 +1,26 @@
+#ifndef VTAB_TABLE_ROW_ID_H
+#define VTAB_TABLE_ROW_ID_H
+
+#include <cstdint>
+
+#include "page/page.h"
+
+class RowID {
+ public:
+  RowID() = default;
+  RowID(int64_t row_id) : page_id_(row_id >> 32), slot_number_(row_id) {}
+
+  void Set(PageID page_id, int32_t slot_number) {
+    page_id_ = page_id;
+    slot_number_ = slot_number;
+  }
+
+  PageID page_id() const { return page_id_; }
+  int32_t slot_number() const { return slot_number_; }
+
+ private:
+  PageID page_id_ = INVALID_PAGE_ID;
+  int32_t slot_number_;
+};
+
+#endif
