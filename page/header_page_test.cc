b7af63c626b8037fc92200dfce5a97c160e532df
invzhi <invzhi@protonmail.com>
2019-04-25 15:26:33 +0800

    page/header_page_test: use EXPECT_TRUE, EXPECT_FALSE

--- page/header_page_test.cc
+++ page/header_page_test.cc
@@ -7,35 +7,35 @@ TEST(HeaderPageTest, SampleTest) {
 
   page.Init();
   // insert
-  EXPECT_EQ(true, page.InsertRecord("table1", 1));
-  EXPECT_EQ(true, page.InsertRecord("table2", 2));
-  EXPECT_EQ(true, page.InsertRecord("table3", 3));
-  EXPECT_EQ(true, page.InsertRecord("table4", 4));
-  EXPECT_EQ(true, page.InsertRecord("table5", 5));
+  EXPECT_TRUE(page.InsertRecord("table1", 1));
+  EXPECT_TRUE(page.InsertRecord("table2", 2));
+  EXPECT_TRUE(page.InsertRecord("table3", 3));
+  EXPECT_TRUE(page.InsertRecord("table4", 4));
+  EXPECT_TRUE(page.InsertRecord("table5", 5));
   // duplicate
-  EXPECT_EQ(false, page.InsertRecord("table1", 1));
+  EXPECT_FALSE(page.InsertRecord("table1", 1));
 
   // get
   PageID page_id;
-  EXPECT_EQ(false, page.GetPageID("notexist", page_id));
-  EXPECT_EQ(true, page.GetPageID("table1", page_id));
+  EXPECT_FALSE(page.GetPageID("notexist", page_id));
+  EXPECT_TRUE(page.GetPageID("table1", page_id));
   EXPECT_EQ(1, page_id);
-  EXPECT_EQ(true, page.GetPageID("table2", page_id));
+  EXPECT_TRUE(page.GetPageID("table2", page_id));
   EXPECT_EQ(2, page_id);
-  EXPECT_EQ(true, page.GetPageID("table3", page_id));
+  EXPECT_TRUE(page.GetPageID("table3", page_id));
   EXPECT_EQ(3, page_id);
-  EXPECT_EQ(true, page.GetPageID("table4", page_id));
+  EXPECT_TRUE(page.GetPageID("table4", page_id));
   EXPECT_EQ(4, page_id);
-  EXPECT_EQ(true, page.GetPageID("table5", page_id));
+  EXPECT_TRUE(page.GetPageID("table5", page_id));
   EXPECT_EQ(5, page_id);
 
   // update
-  EXPECT_EQ(false, page.UpdateRecord("notexist", 30));
-  EXPECT_EQ(true, page.UpdateRecord("table3", 30));
-  EXPECT_EQ(true, page.GetPageID("table3", page_id));
+  EXPECT_FALSE(page.UpdateRecord("notexist", 30));
+  EXPECT_TRUE(page.UpdateRecord("table3", 30));
+  EXPECT_TRUE(page.GetPageID("table3", page_id));
   EXPECT_EQ(30, page_id);
 
   // delete
-  EXPECT_EQ(true, page.DeleteRecord("table3"));
-  EXPECT_EQ(false, page.GetPageID("table3", page_id));
+  EXPECT_TRUE(page.DeleteRecord("table3"));
+  EXPECT_FALSE(page.GetPageID("table3", page_id));
 }
